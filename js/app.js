9890455fec05164893bbf1ed55e795dbc304afcb
Jobin Nelson <jobinnelson369@gmail.com>
2023-04-07 18:47:18 +0530

    made the codebase more declarative

--- js/app.js
+++ js/app.js
@@ -20,32 +20,25 @@ function init() {
   const view = new View()
   const store = new Store('live-t3-storage-key', players)
 
-  function initView() {
-    view.closeAll()
-    view.clearMoves()
-    view.setTurnIndicator(store.game.currentPlayer)
-    view.updateScoreboard(
-      store.stats.playerWithStats[0].wins,
-      store.stats.playerWithStats[1].wins,
-      store.stats.ties,
-    )
-    view.initializeMoves(store.game.moves)
-  }
+  // Current tab state changes
+  store.addEventListener('statechange', () => {
+    view.render(store.game, store.stats)
+  })
 
+  // A different tab state changes
   window.addEventListener('storage', () => {
-    initView()
+    view.render(store.game, store.stats)
   })
 
-  initView()
+  // The first load of the document
+  view.render(store.game, store.stats)
 
-  view.bindGameResetEvent(event => {
+  view.bindGameResetEvent(() => {
     store.reset()
-    initView()
   })
 
-  view.bindNewRoundEvent(event => {
+  view.bindNewRoundEvent(() => {
     store.newRound()
-    initView()
   })
 
   view.bindPlayerMoveEvent(square => {
@@ -53,20 +46,8 @@ function init() {
 
     if (existingMove) { return }
 
-    // Place an icon of the current player in a square
-    view.handlePlayerMove(square, store.game.currentPlayer)
-
     // Advance to the next state by 
     store.playerMove(+square.id)
-
-    // Check winner
-    if (store.game.status.isComplete) {
-      view.openModal(store.game.status.winner ? `${store.game.status.winner.name} wins!` : 'Tie!')
-      return
-    }
-
-    // Set the next player's turn indicator
-    view.setTurnIndicator(store.game.currentPlayer)
   })
 
 }
